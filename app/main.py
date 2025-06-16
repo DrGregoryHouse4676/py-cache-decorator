640b60d00d0c45ff7b08845cde8b71eed103de87
Lev <lev01111989@gmail.com>
2025-06-16 16:35:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    saved_args = []
+    saved_results = []
+
+    def wrapper(*args: Any) -> Any:
+       for i in range(len(saved_args)):
+           if args == saved_args[i]:
+               print("Getting from cache")
+               return saved_results[i]
+
+       result = func(*args)
+       print("Calculating new result")
+       saved_args.append(args)
+       saved_results.append(result)
+       return result
+
+    return wrapper
+
+@cache
+def delay_addition(a, b):
+    import time
+    time.sleep(3)
+    return a + b
